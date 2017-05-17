483c97b0abd963f6111099a6c0ee8e32e265813e
yves chen <yves@yvess-iMac.local>
2017-05-17 15:13:38 +0800

    new version matrix

--- ANN.cpp
+++ ANN.cpp
@@ -9,19 +9,19 @@ class matrix {
     matrix();
     int print();
     int sigmoid();
+    friend martix& operator =(const matrix& m1);
     friend martix operator +(const matrix& m1, const matrix& m2);
     friend martix operator -(const matrix& m1, const matrix& m2);
     friend martix operator *(const matrix& m1, const matrix& m2);
     friend martix operator /(const matrix& m1, const matrix& m2);
     friend martix operator -(const matrix& m1);
     friend bool operator ==(const matrix& m1, const matrix& m2);
-    friend iostream operator >>(istream& ins, matrix& m1);
-    friend iostream operator <<(ostream& ins, matrix& m1);
+    friend istream operator >>(istream& ins, matrix& m1);
+    friend ostream operator <<(ostream& ins, matrix& m1);
   private:
     int row, column;
     double *a;
 };
 int main() {
   matrix mymatix;
-
 }
