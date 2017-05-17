52b258c5de561a9aef92f3bb89c1e0accef2574f
yves chen <yves@yvess-iMac.local>
2017-05-17 17:54:03 +0800

    new matrix finish

--- ANN.cpp
+++ ANN.cpp
@@ -14,7 +14,10 @@ class matrix {
     friend matrix operator +(const matrix& m1, const matrix& m2);
     friend matrix operator -(const matrix& m1, const matrix& m2);
     friend matrix operator *(const matrix& m1, const matrix& m2);
-    friend matrix operator /(const matrix& m1, const matrix& m2);
+    friend matrix operator *(const int& x1, const matrix& m1);
+    friend matrix operator *(const matrix& m1, const int& x1);
+    friend matrix operator /(const int& x1, const matrix& m1);
+    friend matrix operator /(const matrix& m1, const int& x1);
     friend matrix operator -(const matrix& m1);
     friend bool operator ==(const matrix& m1, const matrix& m2);
     friend istream& operator >>(istream& ins,const matrix& m1);
@@ -86,43 +89,135 @@ int matrix::sigmoid() {
   }
   return 0;
 }
-matrix& operator =(const matrix& m1) {
+matrix& matrix::operator =(const matrix& m1) {
+  if(&m1 == this)
+    return *this;
   int i, j;
-  for(i = 0; i < row; i++) {
-      for(j = 0; j < column; j++) {
-          
+  this->row = m1.row;
+  this->column = m1.column;
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m1.column; j++) {
+          this->a[i * m1.column + j] = m1.a[i * m1.column + j];
       }
   }
-  return 0;
+  return *this;
 }
 matrix operator +(const matrix& m1, const matrix& m2) {
-
+  if (m1.row != m2.row || m1.column != m2.column) {
+    cout << "matrix error: operator +" << endl;
+    matrix err(0, 0);
+    return err;
+  }
+  int i, j;
+  matrix answer(m1.row, m1.column);
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m1.column; j++) {
+          answer.a[i * m1.column + j] = m1.a[i * m1.column + j] + m2.a[i * m1.column + j];
+      }
+  }
+  return answer;
 }
 matrix operator -(const matrix& m1, const matrix& m2) {
-
+  if (m1.row != m2.row || m1.column != m2.column) {
+    cout << "matrix error: operator -" << endl;
+    matrix err(0, 0);
+    return err;
+  }
+  int i, j;
+  matrix answer(m1.row, m1.column);
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m1.column; j++) {
+          answer.a[i * m1.column + j] = m1.a[i * m1.column + j] - m2.a[i * m1.column + j];
+      }
+  }
+  return answer;
 }
 matrix operator *(const matrix& m1, const matrix& m2) {
-
+  if (m1.column != m2.row) {
+    cout << "matrix error: operator *" << endl;
+    matrix err(0, 0);
+    return err;
+  }
+  matrix answer(m1.row, m2.column);
+  int i, j, k;
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m2.column; j++) {
+          double temp = 0;
+          for(k = 0; k < m1.column; k++) {
+              temp += m1.a[i * m1.column + k] * m2.a[k * m2.column + j];
+          }
+          answer.a[i * m2.column + j] = temp;
+      }
+  }
+  return answer;
 }
-matrix operator /(const matrix& m1, const matrix& m2) {
-
+matrix operator *(const int& x1, const matrix& m1) {
+  int i, j;
+  matrix answer(m1.row, m1.column);
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m1.column; j++) {
+          answer.a[i * m1.column + j] = x1 * m1.a[i * m1.column + j];
+      }
+  }
+  return answer;
+}
+matrix operator *(const matrix& m1, const int& x1) {
+  int i, j;
+  matrix answer(m1.row, m1.column);
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m1.column; j++) {
+          answer.a[i * m1.column + j] = x1 * m1.a[i * m1.column + j];
+      }
+  }
+  return answer;
+}
+matrix operator /(const int& x1, const matrix& m1) {
+  int i, j;
+  matrix answer(m1.row, m1.column);
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m1.column; j++) {
+          answer.a[i * m1.column + j] = x1 / m1.a[i * m1.column + j];
+      }
+  }
+  return answer;
+}
+matrix operator /(const matrix& m1, const int& x1) {
+  int i, j;
+  matrix answer(m1.row, m1.column);
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m1.column; j++) {
+          answer.a[i * m1.column + j] = m1.a[i * m1.column + j] / x1;
+      }
+  }
+  return answer;
 }
 matrix operator -(const matrix& m1) {
-
+  int i, j;
+  matrix answer(m1.row, m1.column);
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m1.column; j++) {
+          answer.a[i * m1.column + j] = -m1.a[i * m1.column + j];
+      }
+  }
+  return answer;
 }
 bool operator ==(const matrix& m1, const matrix& m2) {
-
-}
-istream& operator >>(istream& ins,const matrix& m1) {
-
-}
-ostream& operator <<(ostream& ins,const matrix& m1) {
-
+  int i, j;
+  bool answer = true;
+  for(i = 0; i < m1.row; i++) {
+      for(j = 0; j < m1.column; j++) {
+          if(m1.a[i * m1.column + j] != m2.a[i * m1.column + j])
+            answer = false;
+      }
+  }
+  return answer;
 }
 int main() {
   matrix mymatix(3, 2);
+  matrix mymatix2(4, 2);
   double a[6] = {1, 2, 3, 4, 5, 6};
   mymatix.setmatrix(3, 2, a);
   mymatix.sigmoid();
-  mymatix.print();
+  mymatix2 = mymatix2 + mymatix;
+  mymatix2.print();
 }
