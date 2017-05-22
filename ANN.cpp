9c2c49ea93ec5ba99d1d2027359c36a3130fa351
yves chen <yves@yvess-iMac.local>
2017-05-23 02:37:11 +0800

    manager function

--- ANN.cpp
+++ ANN.cpp
@@ -37,6 +37,8 @@ class matrix {
     int get_row();
     int get_column();
     int print();
+    int save_to_file(string filename);
+    int load_from_file(string filename);
     double length();
     matrix transfer(double (*function)(double));
     matrix transpose();
@@ -161,7 +163,7 @@ int matrix::print() {
   cout << ">>>" << this->row << " by " << this->column << " matrix." << endl;
   cout << ">>>   ";
   for(j = 0; j < this->column; j++) {
-    cout << "c" << j + 1 <<"     ";
+    cout << "c" << j + 1 <<"    ";
   }
   cout << endl;
   for(i = 0; i < this->row; i++) {
@@ -172,6 +174,21 @@ int matrix::print() {
         cout << this->a[i * this->column + j] << " ";
       }
       cout << endl;
+  }
+  return 0;
+}
+int matrix::save_to_file(string filename) {
+  ofstream myfile (filename + ".mtrx");
+  myfile <<  (*this);
+  return 0;
+}
+int matrix::load_from_file(string filename) {
+  ifstream myfile (filename + ".mtrx");
+  if(myfile.is_open()) {
+      myfile >> (*this);
+  }
+  else {
+    return -1;
   }
   return 0;
 }
@@ -335,7 +352,9 @@ ostream& operator<<(ostream &out, const matrix& m1) {
   out << " " << m1.row << " " << m1.column;
   for(i = 0; i < m1.row; i++) {
       for(j = 0; j < m1.column; j++) {
-          out << " " << m1.a[i * m1.column + j];
+        cout << fixed;
+        cout << setprecision(10);
+        out << " " << m1.a[i * m1.column + j];
       }
   }
   return out;
@@ -366,10 +385,7 @@ class ANN {
     int save_to_file(string filename);
     int load_from_file(string filename);
     int train(matrix input, matrix output, double speed);
-    int train_pro(matrix input, matrix output, double err, int max_times, double speed);
-    int train_pro_graph(matrix input, matrix output, double err, int max_times, double speed);
-    int train_pro_graph_simple(matrix input, matrix output, double err, int max_times, double speed);
-    int train_pro_graph_test(matrix input, matrix output, double err, int max_times, double speed);
+    int train_pro(matrix input, matrix output, double err, int max_times, double speed, int loop);
     matrix feed(matrix input);
   private:
     int layers_size, *neurons_size;
@@ -533,109 +549,19 @@ int ANN::train(matrix input, matrix output,double speed) {
   speed = speed;
   return 0;
 }
-int ANN::train_pro(matrix input, matrix output, double err, int max_times, double speed) {
-  int count = 0;
-  while((this->feed(input) - output.transfer(sigmoid)).length() > err && (count < max_times || max_times == -1)) {
-    this->train(input, output, speed);
-    count ++;
-  }
-  if (count < max_times) {
-    return count;
-  }
-  else {
-    return -1;
-  }
-}
-int ANN::train_pro_graph(matrix input, matrix output, double err, int max_times, double speed) {
-  cout << ">>>***--before_train--***" << endl;
-  this->print();
-  cout << ">>>-----out sigmoid-----" << endl;
-  (output.transfer(sigmoid)).print();
-  cout << ">>>-----feed sigmoid-----" << endl;
-  (this->feed(input)).print();
-  cout << ">>>-----out origin-----" << endl;
-  (output).print();
-  cout << ">>>-----feed origin-----" << endl;
-  ((this->feed(input)).transfer(logit)).print();
-  cout << ">>>-----err value-----" << endl;
-  cout << fixed;
-  cout << setprecision(10);
-  cout << ">>>" << (this->feed(input) - output.transfer(sigmoid)).length() << endl;
-  cout << ">>>***--after_train--***" << endl;
-  double firsterr = (this->feed(input) - output.transfer(sigmoid)).length();
-  int count = 0;
-  while((this->feed(input) - output.transfer(sigmoid)).length() > err && (count < max_times || max_times == -1)) {
-    if(count % 2500 == 0) {
-      for (int i = 0; i < ((this->feed(input) - output.transfer(sigmoid)).length() / firsterr) * 160; i++)
-        cout << "*";
-      cout << endl;
-    }
-    this->train(input, output, speed);
-    count ++;
-  }
-  this->print();
-  cout << ">>>-----out sigmoid-----" << endl;
-  (output.transfer(sigmoid)).print();
-  cout << ">>>-----feed sigmoid-----" << endl;
-  (this->feed(input)).print();
-  cout << ">>>-----out origin-----" << endl;
-  (output).print();
-  cout << ">>>-----feed origin-----" << endl;
-  ((this->feed(input)).transfer(logit)).print();
-  cout << ">>>-----err value-----" << endl;
-  cout << fixed;
-  cout << setprecision(10);
-  cout << ">>>" << (this->feed(input) - output.transfer(sigmoid)).length() << endl;
-  cout << ">>>-----try times-----" << endl;
-  cout << count << endl;
-  if (count < max_times) {
-    return count;
-  }
-  else {
-    return -1;
-  }
-}
-int ANN::train_pro_graph_simple(matrix input, matrix output, double err, int max_times, double speed) {
-  double firsterr = (this->feed(input) - output.transfer(sigmoid)).length();
-  int count = 0;
-  while((this->feed(input) - output.transfer(sigmoid)).length() > err && (count < max_times || max_times == -1)) {
-    if(count % 2500 == 0) {
-      for (int i = 0; i < ((this->feed(input) - output.transfer(sigmoid)).length() / firsterr) * 160; i++)
-        cout << "*";
-      cout << endl;
-    }
-    this->train(input, output, speed);
-    count ++;
-  }
-  this->print();
-  cout << ">>>-----out origin-----" << endl;
-  (output).print();
-  cout << ">>>-----feed origin-----" << endl;
-  ((this->feed(input)).transfer(logit)).print();
-  cout << ">>>-----err value-----" << endl;
-  cout << fixed;
-  cout << setprecision(10);
-  cout << ">>>" << (this->feed(input) - output.transfer(sigmoid)).length() << endl;
-  cout << ">>>-----try times-----" << endl;
-  cout << count << endl;
-  if (count < max_times) {
-    return count;
-  }
-  else {
-    return -1;
-  }
-}
-int ANN::train_pro_graph_test(matrix input, matrix output, double err, int max_times, double speed) {
+int ANN::train_pro(matrix input, matrix output, double err, int max_times, double speed, int loop) {
   ANN good;
   double speed_max = speed;
+  // this->train(input, output, speed);
   double good_err = 99999, firsterr = (this->feed(input) - output.transfer(sigmoid)).length();
+  cout << "firsterr: " << firsterr << endl;
   int count = 0;
   while((this->feed(input) - output.transfer(sigmoid)).length() > err && (count < max_times || max_times == -1)) {
-    if(count % 2500 == 0) {
+    if(count % loop == 0) {
       for (int i = 0; i < ((this->feed(input) - output.transfer(sigmoid)).length() / firsterr) * 160; i++)
         cout << "*";
       if ( good_err == (this->feed(input) - output.transfer(sigmoid)).length()) {
-        cout << " s";
+        cout << " stucked";
         speed =  double( rand() % (int(speed_max + 1))  - (rand() % 99999) * 0.00001);
       }
       if ( good_err > (this->feed(input) - output.transfer(sigmoid)).length()) {
@@ -643,11 +569,12 @@ int ANN::train_pro_graph_test(matrix input, matrix output, double err, int max_t
         good = (*this);
       }
       if ( good_err < (this->feed(input) - output.transfer(sigmoid)).length()) {
-        cout << " b";
+        cout << " bad";
         (*this) = good;
         speed =  double( rand() % (int(speed_max + 1))  - (rand() % 99999) * 0.00001);
       }
       cout << endl;
+      this->save_to_file("latest");
     }
     this->train(input, output, speed);
     count ++;
@@ -685,109 +612,141 @@ matrix ANN::feed(matrix input) {
   }
   return a1;
 }
-int test() {
-  double a[4] = {1, 2, 3, 4};
-  matrix A;
-  int ann[4] = {3, 2 ,1 ,3};
-  ANN myann;
-  myann.load_from_file("test");
-  myann.print();
+int manager () {
+  while(1) {
+    string ann_name;
+    char cmd;
+    bool secmenu = 0;
+    ANN myann;
+    cout << "Artificial neural network (ANN) manager. ver 1.0" << endl;
+    cout << "copyright(c)2017 MAGNET inc." << endl;
+    cout << "For more information \"www.nooxy.tk\"." << endl;
+    cout << "1. Create new ANN (c). 2. Load from old (l). 3. recover from latest train(r) 4. Create matrix(.mtrx) (m) 5. exit (e)." << endl;
+    cin >> cmd;
+    switch (cmd) {
+      case 'c':
+      {
+        int layers_size;
+        cout << "Input \"ANN's name.\"" << endl;
+        cin >> ann_name;
+        cout << "Input \"ANN's layers size.\"" << endl;
+        cin >> layers_size;
+        int neurons_size[layers_size];
+        for(int i = 0; i < layers_size; i++) {
+          cout << "Input \"layer (" << i + 1 << "/" << layers_size << ") neurons size\"." << endl;
+          cin >> neurons_size[i];
+        }
+        ANN temp(layers_size, neurons_size);
+        myann = temp;
+        myann.save_to_file(ann_name);
+        secmenu = 1;
+        break;
+      }
+      case 'l':
+      {
+        cout << "Input \"ANN's name\"." << endl;
+        cin >> ann_name;
+        myann.load_from_file(ann_name);
+        secmenu = 1;
+        break;
+      }
+      case 'r':
+      {
+        cout << "Input \"ANN's name\"." << endl;
+        cin >> ann_name;
+        myann.load_from_file("latest");
+        myann.save_to_file(ann_name);
+        secmenu = 1;
+        break;
+      }
+      case 'm':
+      {
+        string matrix_name;
+        matrix M;
+        cout << "Input \"matrix name(probably \"in\"/\"out\")\", \"row(probably the number of data amount)\", \"column(probably the number of input or output layer\'s neuron size)\"" << endl;
+        cout << "And then input \"elements\" row after row." << endl;
+        cin >> matrix_name >> M;
+        M.save_to_file(matrix_name);
+        break;
+      }
+      case 'e':
+      {
+        return 0;
+        break;
+      }
+      default :
+      {
+        return 0;
+        break;
+      }
+    }
+    cout << "Your ANN : " << ann_name << endl;
+    myann.print();
+    while (secmenu) {
+      char cmd2;
+      cout <<"1. Train (t). 2. Feed (f). 3.Help (h) 4. save (s) 5. back (b)." << endl;
+      cin >> cmd2;
+      switch (cmd2) {
+        case 't':
+        {
+          matrix IN, OUT;
+          if (IN.load_from_file("in") || OUT.load_from_file("out")) {
+            cout << "error: \"in.mtrx\" or \"out.mtrx\" not found. Please create it first." << '\n';
+          }
+          else {
+            double speed, min_err;
+            int times, loop;
+            cout << "Input \"min err value(0.1)\", \"speed(3)\" , \"max training times (-1 for infinite)\", \"times per loop(2500)\"." << '\n';
+            cin >> min_err >> speed >> times >> loop;
+            myann.train_pro(IN, OUT, min_err, times, speed, loop);
+          }
+          break;
+        }
+        case 'f':
+        {
+          matrix FEED;
+          cout << "input \"row(number of data amount)\", \"column(number of input layer\'s neuron size)\"" << endl;
+          cout << "And then input \"elements\" row after row." << endl;
+          cin >> FEED;
+          cout << "result:" << endl << myann.feed(FEED);
+          break;
+        }
+        case 'h':
+        {
+          cout << "You need in.mtrx and out.mtrx to train. Can simply create by tool provide from us from first menu." << endl;
+          cout << "Or you can manually create it by text editor. Form showing below." << endl;
+          cout << "\"row(probably the number of data amount)\", \"column(probably the number of input or output layer\'s neuron size)\"" << endl;
+          cout << "And then input \"elements\" row after row." << endl;
+          cout << "Beware each thing should seperated by space." << endl;
+          cout << "For example 2 * 3 matrix." << endl;
+          cout << "                  input / output neuron size (column\'s size)" << endl;
+          cout << "                                   1 2" << endl;
+          cout << " amount of data (row\'s size)       3 4" << endl;
+          cout << "                                   5 6" << endl;
+          cout << "In .mtrx file should be like" << endl;
+          cout << "2 3 1 2 3 4 5 6" << endl;
+          break;
+        }
+        case 's':
+        {
+          myann.save_to_file(ann_name);
+          break;
+        }
+        case 'b':
+        {
+          secmenu = 0;
+          break;
+        }
+        default :
+        {
+          secmenu = 0;
+          break;
+        }
+      }
+    }
+  }
+  return 0;
 }
 int main() {
-  test();
-  // int neurons_size[50] = {64, 128, 128, 64};
-  // double
-  // // x[1000] =
-  // // { 0, 0, 0, 0,
-  // //   0, 0, 0, 1,
-  // //   0, 0, 1, 0,
-  // //   0, 0, 1, 1,
-  // //   0, 1, 0, 0,
-  // //   0, 1, 0, 1,
-  // //   0, 1, 1, 0,
-  // //   0, 1, 1, 1,
-  // //   1, 0, 0, 0,
-  // //   1, 0, 0, 1,
-  // //   1, 0, 1, 0,
-  // //   1, 0, 1, 1,
-  // //   1, 1, 0, 0,
-  // //   1, 1, 0, 1,
-  // // },
-  // // y[1000] =
-  // // { 0, 0, 0, 1,
-  // //   0, 0, 1, 0,
-  // //   0, 0, 1, 1,
-  // //   0, 1, 0, 0,
-  // //   0, 1, 0, 1,
-  // //   0, 1, 1, 0,
-  // //   0, 1, 1, 1,
-  // //   1, 0, 0, 0,
-  // //   1, 0, 0, 1,
-  // //   1, 0, 1, 0,
-  // //   1, 0, 1, 1,
-  // //   1, 1, 0, 0,
-  // //   1, 1, 0, 1,
-  // //   1, 1, 1, 0,
-  // // };
-  // x[1000] =
-  // { 0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 1, -1, 0, 0, 0,
-  //   0, 0, 0, -1, 1, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, -1, 1, 0, 0, 0,
-  //   0, 0, 0, -1, 1, 0, 0, 0,
-  //   0, 0, 0, -1, 1, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  // },
-  // y[1000] =
-  // { 0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 1, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 1, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  //   0, 0, 0, 0, 0, 0, 0, 0,
-  // };
-  // matrix X, Y;
-  // X.setmatrix(2, 64, x);
-  // Y.setmatrix(2, 64, y);
-  // ANN myann(4, neurons_size);
-  // myann.randomweight();
-  // myann.train_pro_graph_test(X, Y, 0.1, -1, 1);
-  // // cout << "feed 0 0 0 0 (answer 0 0 0 1)" << endl;
-  // // double p1[4] = {0, 0, 0, 0};
-  // // matrix P1(1, 4, p1);
-  // // (myann.feed(P1).transfer(logit)).print();
-  // // cout << "feed 1 0 1 0 (answer 1 0 1 1)" << endl;
-  // // double p2[4] = {1, 0, 1, 0};
-  // // matrix P2(1, 4, p2);
-  // // (myann.feed(P2).transfer(logit)).print();
-  // // cout << "feed 0 1 1 0 (answer 0 1 1 1)" << endl;
-  // // double p3[4] = {0, 1, 1, 0};
-  // // matrix P3(1, 4, p3);
-  // // (myann.feed(P3).transfer(logit)).print();
-  // // cout << "predict 1 1 1 0 (answer 1 1 1 1)" << endl;
-  // // double p4[4] = {1, 1, 1, 0}, p4o[4] = { 1, 1, 1, 1} ;
-  // // matrix P4(1, 4, p4);matrix P4O(1, 4, p4o);
-  // // cout << "err: " << (myann.feed(P4).transfer(logit) -  P4O).length() << endl;
-  // // (myann.feed(P4).transfer(logit)).print();
+  manager();
 }
