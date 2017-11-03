7677c9e5005b1cb108176adaa9c1aef5e2f7e0d3
Yves Chen <magneticchen@gmail.com>
2017-11-03 21:53:15 +0800

    do some reversi stuff

--- research/reversi/scripts/reversi.py
+++ research/reversi/scripts/reversi.py
@@ -1,6 +1,9 @@
 table = {'A': 'O', 'B': '@', 'N': ' '}
+Input = []
+Output = []
+
 class ReversiUtility(object):
-    def printkey(key):
+    def printKey(key):
         sublocation = 0
         for location in key:
             print(table[location]+' ', end='')
@@ -9,4 +12,44 @@ class ReversiUtility(object):
                 sublocation = 0
             else:
                 sublocation += 1
-ReversiUtility.printkey('NNNNNNNNNNNNNNNNNNNNNNNNNNNABNNNNNAAANNNNNNNNNNNNNNNNNNNNNNNNNNN')
+
+    def getPointbyKey(key):
+        userpoint = 0
+        compoint = 0
+        for location in key:
+            if location == 'A':
+                userpoint += 1
+            elif location == 'B':
+                compoint += 1
+            else:
+                pass
+        return userpoint, compoint
+
+    def appendData(In, Out):
+        Input.append(In)
+        Output.append(Out)
+
+    def dumpDatatomtrx(Filename):
+        length = len(Input)
+        Inputstring = str(length)+' 64\n'
+        Outputstring = str(length)+' 64\n'
+        for x in Input:
+            for y in x:
+                if y == 'A':
+                    Inputstring += ' '
+                elif y == 'B':
+                    Inputstring += ' '
+                else:
+        # Input first
+    def turnKey90degree(key):
+        pass
+
+    def mirrorKeyXaxis(Key):
+        pass
+
+    def mirrorKeyYaxis(Key):
+        pass
+    def reverseKey(Key):
+        pass
+x = ReversiUtility.getPointbyKey('BBBBBBBBBNABBBBBBBAAABBBBABAAABBBAABAAABBABABAABBAABAAABBABBBBBB')
+print(x)
