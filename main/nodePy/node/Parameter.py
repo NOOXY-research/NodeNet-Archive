8fabc5dc0974624f504e2e7df4516d12b18d9f45
Yves Chen <magneticchen@gmail.com>
2017-11-07 17:49:32 +0800

    update version tag

--- main/nodePy/node/Parameter.py
+++ main/nodePy/node/Parameter.py
@@ -1,7 +1,7 @@
 # Parameter.py shares variables that be refered globally
 import node.NeuralNetwork.LearningAlgorithm as LA
 
-NODEPY_VERSION = 'Python aphla 1.1.2'
+NODEPY_VERSION = 'Python aphla 1.1.3'
 VERBOSE_PER_LOOP_DEFAULT = 10000
 SAVED_PATH = 'saved/'
 DATA_PATH = 'data/'
